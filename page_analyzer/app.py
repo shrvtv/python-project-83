cd1abceab153b05bcf2072ee0ed649575e79c5a3
Artem Sharovatov <git@sharovatov.me>
2025-08-28 12:39:41 +0300

    Add dotenv

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask("page_analyzer")
-
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route("/")
 def hello_world():
