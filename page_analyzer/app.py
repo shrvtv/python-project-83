ab39f68db89575f7a04dfce0f1311336a3f968c8
Artem Sharovatov <git@sharovatov.me>
2025-08-30 21:42:29 +0300

    Add stub for SQL db connection

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
+import psycopg2
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+# conn = psycopg2.connect(DATABASE_URL)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.route("/urls")
 def urls():
