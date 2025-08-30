813d8f810d384e5d4e796251a54b69a7fc037556
Artem Sharovatov <git@sharovatov.me>
2025-08-30 21:30:18 +0300

    Add GET /urls route

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 from dotenv import load_dotenv
 
@@ -9,3 +9,8 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/urls")
+def urls():
+    return render_template("urls.html")
