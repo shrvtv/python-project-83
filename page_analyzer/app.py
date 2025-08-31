a26da47fcb808cc30def42fbe6a24175ed295d7c
Artem Sharovatov <git@sharovatov.me>
2025-08-31 22:01:11 +0300

    Add URL validation

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, flash
+from validators.url import url as is_valid_url
 import os
 from dotenv import load_dotenv
 import psycopg2
@@ -33,13 +34,17 @@ def website(website_id):
 
 @app.post("/urls")
 def add_url():
-    url = request.args.get('url')
-    with psycopg2.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
-            cur.execute("""
-                INSERT INTO urls (name)
-                VALUES (%s)
-                RETURNING id;
-            """, (url,))
-            website_id = cur.fetchone()[0]
-    return redirect(url_for("website", website_id=website_id))
+    url = request.form.get('url')
+    if is_valid_url(url) and len(url) <= 255:
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                    INSERT INTO urls (name)
+                    VALUES (%s)
+                    RETURNING id;
+                """, (url,))
+                website_id = cur.fetchone()[0]
+        return redirect(url_for("website", website_id=website_id))
+    else:
+        flash("Некорректный URL")
+        return render_template(url_for("index"))
