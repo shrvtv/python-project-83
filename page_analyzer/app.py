c7bbdd13c07555098f93f4fbb3b36cec826aabfd
Artem Sharovatov <git@sharovatov.me>
2025-08-31 09:09:16 +0300

    Enable Postgres connection

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,7 +5,7 @@ import psycopg2
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-# conn = psycopg2.connect(DATABASE_URL)
+conn = psycopg2.connect(DATABASE_URL)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
