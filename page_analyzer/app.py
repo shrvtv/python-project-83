2d3e9b75444497f4b5689c65ed35c3bbdb27461e
Artem Sharovatov <git@sharovatov.me>
2025-08-31 09:54:12 +0300

    Add DB creation and connection

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -5,9 +5,18 @@ import psycopg2
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+with psycopg2.connect(DATABASE_URL) as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS urls (
+                id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                name VARCHAR(255),
+                created_at DATE
+            )
+        """)
 
 @app.route("/")
 def index():
