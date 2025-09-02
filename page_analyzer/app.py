6b484a7e52d684dcb045839de6c5cbb007e71b41
Artem Sharovatov <git@sharovatov.me>
2025-09-02 12:11:41 +0300

    Make website name mandatory

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -15,7 +15,7 @@ with psycopg2.connect(DATABASE_URL) as conn:
         cur.execute("""
             CREATE TABLE IF NOT EXISTS urls (
                 id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-                name VARCHAR(255) UNIQUE,
+                name VARCHAR(255) UNIQUE NOT NULL,
                 created_at DATE NOT NULL DEFAULT CURRENT_DATE
             );
         """)
