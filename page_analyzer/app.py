6c9addb7ae7c17959555eeb16cbccb58ae410ae4
Artem Sharovatov <git@sharovatov.me>
2025-09-02 12:59:12 +0300

    Isolate DB init from the rest of the code

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -10,23 +10,40 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-with psycopg2.connect(DATABASE_URL) as conn:
-    with conn.cursor() as cur:
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS urls (
-                id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-                name VARCHAR(255) UNIQUE NOT NULL,
-                created_at DATE NOT NULL DEFAULT CURRENT_DATE
-            );
-        """)
+
+def init_db():
+    with psycopg2.connect(DATABASE_URL) as conn:
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS urls (
+                    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                    name VARCHAR(255) UNIQUE NOT NULL,
+                    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+                );
+            """)
+        with conn.cursor() as cur:
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS url_checks (
+                    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                    url_id BIGINT REFERENCES urls(id) ON DELETE CASCADE NOT NULL,
+                    status_code SMALLINT,
+                    h1 VARCHAR(255),
+                    title VARCHAR(255),
+                    description TEXT,
+                    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+                );
+            """)
+
 
 @app.route("/")
 def index():
+    init_db()
     return render_template("index.html")
 
 
 @app.get("/urls")
 def urls():
+    init_db()
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
             cur.execute("SELECT id, name FROM urls ORDER BY id DESC;")
@@ -36,6 +53,7 @@ def urls():
 
 @app.route("/urls/<int:url_id>")
 def website(url_id):
+    init_db()
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor(cursor_factory=NamedTupleCursor) as cur:
             cur.execute(
@@ -50,6 +68,7 @@ def website(url_id):
 
 @app.post("/urls")
 def add_url():
+    init_db()
     url = request.form.get('url')
     if not (is_valid_url(url) and len(url) <= 255):
         flash("Некорректный URL")
