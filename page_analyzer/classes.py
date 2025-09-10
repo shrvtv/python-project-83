b66376731da91d109638648d30636764ba502f34
Artem Sharovatov <git@sharovatov.me>
2025-09-10 12:44:42 +0300

    Add docstrings to all classes and methods in classes.py

--- page_analyzer/classes.py
+++ page_analyzer/classes.py
@@ -7,6 +7,7 @@ import page_analyzer.utils as utils
 
 @dataclass(slots=True)
 class URL:
+    """Dataclass for storing URL data."""
     id: Final[int]
     name: Final[str]
     created_at: Final[datetime.date]
@@ -16,6 +17,7 @@ class URL:
 
 @dataclass(frozen=True, slots=True)
 class Check:
+    """Immutable dataclass for storing results of a URL check."""
     id: int
     url_id: int
     status_code: int
@@ -26,10 +28,18 @@ class Check:
 
 
 class Repository:
+    """Abstraction for interaction with tables 'urls' and 'url_checks'
+    in a specified database. Relies on connection and DictCursor provided by
+    dict_cursor function from 'utils' module.
+    """
     def __init__(self, dsn: str) -> None:
+        """Create a Repository object with a specific Data Source Name
+        for connecting to a database.
+        """
         self.dsn: Final[str] = dsn
     
     def get_all_urls(self) -> list[URL]:
+        """Extract all URLs data from the 'urls' table."""
         with utils.dict_cursor(self.dsn) as cur:
             cur.execute("""
                 SELECT id, name, created_at, last_check, last_status_code
@@ -39,6 +49,7 @@ class Repository:
             return [URL(**row) for row in cur.fetchall()]
 
     def get_all_checks(self, url_id: int) -> list[Check]:
+        """Extract all URL checks data from the 'url_checks' table."""
         with utils.dict_cursor(self.dsn) as cur:
             cur.execute("""
                 SELECT id, url_id, status_code, h1,
@@ -52,6 +63,7 @@ class Repository:
             return [Check(**row) for row in cur.fetchall()]
 
     def save_url(self, name: str) -> URL:
+        """Save a URL to the 'urls' table and return a URL object."""
         with utils.dict_cursor(self.dsn) as cur:
             cur.execute("""
                 INSERT INTO urls (name) VALUES (%s)
@@ -69,6 +81,9 @@ class Repository:
         title: str | None,
         description: str | None
     ) -> Check:
+        """Save url check data to the 'url_checks' table and
+        return a Check object.
+        """
         with utils.dict_cursor(self.dsn) as cur:
             cur.execute("""
                 INSERT INTO url_checks
@@ -82,6 +97,9 @@ class Repository:
             return Check(**cur.fetchone())
 
     def find_url_by_name(self, name: str) -> URL | None:
+        """Find a URL in the 'urls' table by the former's name and
+        return a URL object if such an entry exists or None if it doesn't.
+        """
         with utils.dict_cursor(self.dsn) as cur:
             cur.execute("""
                 SELECT id, name, created_at, last_check, last_status_code
@@ -94,6 +112,9 @@ class Repository:
             return URL(**data) if data else None
     
     def find_url_by_id(self, url_id: int) -> URL | None:
+        """Find a URL in the 'urls' table by the former's id and
+        return a URL object if such an entry exists or None if it doesn't.
+        """
         with utils.dict_cursor(self.dsn) as cur:
             cur.execute("""
                 SELECT id, name, created_at, last_check, last_status_code
@@ -106,11 +127,12 @@ class Repository:
             return URL(**data) if data else None
         
     def update_url(self, url: URL) -> None:
+        """Update a URL in the 'urls' table with data from last check."""
         with utils.dict_cursor(self.dsn) as cur:
             cur.execute("""
                 UPDATE urls
-                SET name = %s, last_check = %s, last_status_code = %s
+                SET last_check = %s, last_status_code = %s
                 WHERE id = %s;
                 """,
-                (url.name, url.last_check, url.last_status_code, url.id)
+                (url.last_check, url.last_status_code, url.id)
             )
